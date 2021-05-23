4c6801f9ab6bab383825d51c5056d9b44bf29c1e
Sergey Kharchenko <hidden>
2021-05-23 14:58:20 +0100

    Make Gather Fixture Setup attribute as Assembly attribute
    1. Mark GatherFixtureSetupLogsAttribute as Assembly target
    
    2. Refatoring of GatherFixtureSetupLogsAttribute class
    
    3. Add template for Global OneTimeSetUp using case

---
+++ tests/NUnit.TestFixtureLogger.Tests/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using NUnit.TestFixtureLogger.Attributes;
+
+[assembly: GatherFixtureSetupLogs]
---
+++ tests/NUnit.TestFixtureLogger.Tests/TestsWithGlobalSetUp/GlobalSetup.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Serilog;
+using System;
+
+namespace NUnit.TestFixtureLogger.Tests.TestsWithGlobalSetUp
+{
+    [SetUpFixture]
+    public class GlobalSetup
+    {
+        [OneTimeSetUp]
+        public static void GlobalOneTimeSetup()
+        {
+            using var logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+            logger.Information($"{nameof(GlobalOneTimeSetup)} of {nameof(GlobalSetup)}");
+
+            throw new Exception("Global OneTimeSetup for sub-set of test suites.");
+        }
+    }
+}
