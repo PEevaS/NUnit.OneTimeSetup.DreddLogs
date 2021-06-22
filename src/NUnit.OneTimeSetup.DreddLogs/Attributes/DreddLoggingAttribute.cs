392e618a875e23767b98cd8218cdc1e844167acf
alexsanv <alexsanv94@yandex.ru>
2021-06-22 11:15:50 +0300

    fix AmbiguousMatchException

--- src/NUnit.OneTimeSetup.DreddLogs/Attributes/DreddLoggingAttribute.cs
+++ src/NUnit.OneTimeSetup.DreddLogs/Attributes/DreddLoggingAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using AspectInjector.Broker;
@@ -20,7 +21,7 @@ namespace NUnit.OneTimeSetup.DreddLogs.Attributes
             [Argument(Source.ReturnType)] Type returnType)
         {
 
-            if (IsFixtureSetupMethod(target.Method.ReflectedType, methodName) is false)
+            if (IsFixtureSetupMethod(target.Method.ReflectedType, methodName, args) is false)
             {
                 return target(args);
             }
@@ -33,9 +34,42 @@ namespace NUnit.OneTimeSetup.DreddLogs.Attributes
             return Wrap(target, args);
         }
 
-        private bool IsFixtureSetupMethod(Type reflectedType, string methodName)
+        private bool IsFixtureSetupMethod(Type reflectedType, string methodName, object[] parameters)
         {
-            return reflectedType.GetMethod(methodName).GetCustomAttribute<OneTimeSetUpAttribute>() != null;
+            var methodInfo = FindMethod(reflectedType, methodName, parameters);
+            return methodInfo.GetCustomAttribute<OneTimeSetUpAttribute>() != null;
+        }
+
+        private MethodInfo FindMethod(Type reflectedType, string methodName, object[] parameters)
+        {
+            var methods = reflectedType.GetMethods()
+                .Where(m => m.Name == methodName)
+                .ToList();
+
+            if (methods.Count() == 1)
+            {
+                return methods.First();
+            }
+
+            return methods.First(m =>
+            {
+                var methodParameters = m.GetParameters();
+
+                if (methodParameters.Count() == parameters.Count())
+                {
+                    for (int i = 0; i < methodParameters.Length; i++)
+                    {
+                        if (methodParameters[i].ParameterType != parameters[i].GetType())
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+
+                return false;
+            });
         }
 
         private bool IsAsyncMethod(Type returnType)
---
+++ tests/NUnit.OneTimeSetup.DreddLogs.Tests/OverloadedMethodsTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using NUnit.OneTimeSetup.DreddLogs.Attributes;
+using NUnit.OneTimeSetup.DreddLogs.Tests.Internal;
+
+namespace NUnit.OneTimeSetup.DreddLogs.Tests
+{
+    [DreddLogging]
+    [TestFixture]
+    public class OverloadedMethodsTests : BaseTest
+    {
+        [TestCase("hello")]
+        [TestCase("world")]
+        public void ParameterizedTest(string str)
+        {
+            OverloadedMethod(str);
+        }
+
+        public void OverloadedMethod()
+        {
+            System.Console.WriteLine("hello world");
+        }
+
+        public void OverloadedMethod(string str)
+        {
+            System.Console.WriteLine(str);
+        }
+    }
+}
